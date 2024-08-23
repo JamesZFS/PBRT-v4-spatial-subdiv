efecf46e7e6dbcabc222868e5cc01b9f4a98c645
Fengshi Zheng <F.Zheng-1@tudelft.nl>
2024-08-23 18:02:09 +0200

    Ubuntu 24 compatibility

---
+++ setpath.sh
@@ -0,0 +1,10 @@
+# This script sets the path to the installed binaries and libraries.
+
+PBRT_DIR=$( realpath $( dirname "${BASH_SOURCE[0]}" ))
+
+if [[ "$(uname)" == 'Darwin' ]]; then
+	export PATH="$PBRT_DIR/build:$PATH"
+else
+	export LD_LIBRARY_PATH="$PBRT_DIR/build"
+	export PATH="$PBRT_DIR/build:$PATH"
+fi
