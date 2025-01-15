c293a2dfb97b41cfd4747c1edf8baa849a754807
Fengshi Zheng <F.Zheng-1@tudelft.nl>
2025-01-15 20:24:58 +0100

    [visualization] show the id of the finest level; show highest energy along a candidate split path

--- src/pbrt/cpu/integrators.h
+++ src/pbrt/cpu/integrators.h
@@ -272,6 +272,7 @@ class GuidedPathIntegrator : public RayIntegrator {
         int treemaxdepthwithsamplecount {12};
         int treelookaheaddepth {3};
         float treeadaptivethreshold {0.1f};
+        float treestdmultiplier {1.0f};
         float treecedecay {0.0f};
         float treececlampvalue {1e8f};
         bool treeenablepromotion {true};
