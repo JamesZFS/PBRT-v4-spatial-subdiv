99aa699f1a1ea8d81c5e433cfb6c39136f9cd963
Fengshi Zheng <F.Zheng-1@tudelft.nl>
2025-02-12 11:13:16 +0100

    [signature] splat+reproject

--- src/pbrt/guiding/Application.cpp
+++ src/pbrt/guiding/Application.cpp
@@ -1435,7 +1435,7 @@ void Application::SpatialSubdivisionSettings() {
         m_subdivCfg.minSamplesPromotion = std::max(0, minSamplesPromotion);
         ImGui::Checkbox("Enable Promotion", &m_subdivCfg.enablePromotion);
         ImGui::Checkbox("Multiply Cosine", &m_subdivCfg.multiplyCosine);
-        ImGui::Combo("Contrib Type", reinterpret_cast<int *>(&m_subdivCfg.contribType), "Determ\0Jitter\0Splat\0Reproject\0");
+        ImGui::Combo("Contrib Type", reinterpret_cast<int *>(&m_subdivCfg.contribType), "Determ\0Jitter\0Splat\0Reproject\0Splat+Reproject\0");
         int lookaheadDepth = (int) m_subdivCfg.lookaheadDepth;
         _(), ImGui::InputInt("Lookahead Depth", &lookaheadDepth, 1, 3);
         m_subdivCfg.lookaheadDepth = std::max(1, std::min(10, lookaheadDepth));
