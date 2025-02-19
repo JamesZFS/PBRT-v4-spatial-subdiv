245eff112c05b97ddf75232e44a07baba26244ff
Fengshi Zheng <F.Zheng-1@tudelft.nl>
2025-02-19 11:07:32 +0100

    [signature] fix discontinuity issues for splat and basis

--- src/pbrt/guiding/RadianceView.cpp
+++ src/pbrt/guiding/RadianceView.cpp
@@ -107,6 +107,30 @@ static float fract(float x) {
     return x - std::floor(x);
 }
 
+// Implements wrapping of k-bit indices in a way that is compatible with
+// octahedral maps
+inline static void wrap(uint32_t &x, uint32_t &y, uint32_t res) {
+    const uint32_t hres = res >> 1;
+    if (x > hres && (y == 0u || y == res)) {
+        x = res - x;
+    }
+    if (y > hres && (x == 0u || x == res)) {
+        y = res - y;
+    }
+}
+
+// Wrapping for splatting
+inline static void wrap_splat(int &x, int &y, uint32_t res) {
+    if (x < 0 || x >= res) {
+        x = std::clamp(x, 0, (int)res - 1);
+        y = res - 1 - y;
+    }
+    if (y < 0 || y >= res) {
+        y = std::clamp(y, 0, (int)res - 1);
+        x = res - 1 - x;
+    }
+}
+
 void RadianceView::UpdateBasisBuffer() {
     Bounds2i pixelBounds = m_camera->GetFilm().PixelBounds();
     auto frame = Frame::FromZ(m_prev.normal);
@@ -115,6 +139,9 @@ void RadianceView::UpdateBasisBuffer() {
     const uint8_t octave_min = pglGetOctaveMin(), octave_max = pglGetOctaveMax();
     const float sigma = pglGetSplatSigma();
     const auto contribType = m_parent->GetSubdivCfg().contribType;
+    const float basis_normalizer = pow(2.0, 1.0 - float(octave_min)) - pow(0.5, float(octave_max));
+    const float alpha = -0.5f / (sigma*sigma);
+    const float kernel_lb = std::exp(alpha);
     for (Point2i p: pixelBounds) {
         float theta = m_stepTheta * (0.5f + float(p.y));
         float phi = m_stepPhi * (0.5f + float(p.x));
@@ -138,16 +165,24 @@ void RadianceView::UpdateBasisBuffer() {
                 m_basisBuffer[j][pixel_index] = 0.0;
             // Iterate over all octaves
             for (uint8_t k = octave_min; k <= octave_max; ++k) {
+                const uint32_t res = 1 << k;
+                const float scale = pow(0.5, float(k)) / basis_normalizer;
                 // Discretize uv at the appropriate resolution
-                pgl_vec2f octave_uv = {uv.x * float(1u << k), uv.y * float(1u << k)};
-                uint32_t x0 = uint32_t(octave_uv.x), y0 = uint32_t(octave_uv.y);
-                // Generate offset versions with wrapping
-                uint32_t x1 = (x0 + 1u) & ((1u << k) - 1u);
-                uint32_t y1 = (y0 + 1u) & ((1u << k) - 1u);
-                uint8_t h00 = pcg_3d(x0, y0, k) % S;
-                uint8_t h01 = pcg_3d(x0, y1, k) % S;
-                uint8_t h10 = pcg_3d(x1, y0, k) % S;
-                uint8_t h11 = pcg_3d(x1, y1, k) % S;
+                pgl_vec2f octave_uv = {uv.x * float(res), uv.y * float(res)};
+                uint32_t x00 = uint32_t(octave_uv.x), y00 = uint32_t(octave_uv.y);
+                // Generate offsets
+                uint32_t x01 = x00, y01 = y00 + 1;
+                uint32_t x10 = x00 + 1, y10 = y00;
+                uint32_t x11 = x00 + 1, y11 = y00 + 1;
+                // Apply wrapping to ensure continuity on the sphere domain
+                wrap(x00, y00, res);
+                wrap(x01, y01, res);
+                wrap(x10, y10, res);
+                wrap(x11, y11, res);
+                uint8_t h00 = pcg_3d(x00, y00, k) % S;
+                uint8_t h01 = pcg_3d(x01, y01, k) % S;
+                uint8_t h10 = pcg_3d(x10, y10, k) % S;
+                uint8_t h11 = pcg_3d(x11, y11, k) % S;
 
                 for (uint8_t j = 0; j < S; ++j) {
                     // Determine whether this bin gets the sample
@@ -160,7 +195,7 @@ void RadianceView::UpdateBasisBuffer() {
                     float M1 = mix(M10, M11, fract(octave_uv.y));
                     float M = mix(M0, M1, fract(octave_uv.x));
                     // Accumulate into the result
-                    m_basisBuffer[j][pixel_index] += pow(0.5, float(k)) / (pow(2.0, 1.0 - float(octave_min)) - pow(0.5, float(octave_max))) * M;
+                    m_basisBuffer[j][pixel_index] += scale * M;
                 }
             }
             // // Check sum
@@ -176,8 +211,6 @@ void RadianceView::UpdateBasisBuffer() {
 
             // Splatting
             // 3x3 Gaussian kernel
-            const float alpha = -0.5f / (sigma*sigma);
-
             constexpr pgl_vec2i offsets[9] = {
                 {-1, -1}, {0, -1}, {+1, -1},
                 {-1,  0}, {0,  0}, {+1,  0},
@@ -195,17 +228,16 @@ void RadianceView::UpdateBasisBuffer() {
 
             float sumCoeff = 0;
             for (int i = 0; i < 9; ++i) {
-                pgl_vec2i qi = {
-                    std::clamp(pi.x + offsets[i].x, 0, (int)oct_res - 1), 
-                    std::clamp(pi.y + offsets[i].y, 0, (int)oct_res - 1)
-                };
-                uint8_t j = pcg_2d(qi.x, qi.y) % S;  // hash to bin
-                
+                pgl_vec2i qi = {pi.x + offsets[i].x, pi.y + offsets[i].y};
                 // pgl_vec2f delta = {(float)(pi.x - qi.x), (float)(pi.y - qi.y)}; // old approach: static weights
                 pgl_vec2f delta = {uv.x * oct_res - (qi.x + 0.5f), uv.y * oct_res - (qi.y + 0.5f)};
-                float coeff = std::exp(alpha * (delta.x*delta.x + delta.y*delta.y));
-                m_basisBuffer[j][pixel_index] += coeff;
+                float coeff = std::max(std::exp(alpha * (delta.x*delta.x + delta.y*delta.y)) - kernel_lb, 0.0f);
                 sumCoeff += coeff;
+                
+                // uint8_t j = pcg_2d(qi.x, qi.y) % S;  // hash to bin
+                wrap_splat(qi.x, qi.y, oct_res);
+                uint8_t j = pcg_2d(qi.x, qi.y) % S;  // hash to bin
+                m_basisBuffer[j][pixel_index] += coeff;
             }
 
             // Normalize weights
