f588d52db8b8c32c897b3ffeca4bb7eb1f44f1d9
Fengshi Zheng <F.Zheng-1@tudelft.nl>
2025-03-06 19:19:45 +0100

    [signature] introduce risk tolerance to prevent noisy splits

--- src/pbrt/guiding/Application.cpp
+++ src/pbrt/guiding/Application.cpp
@@ -1540,6 +1540,7 @@ void Application::SpatialSubdivisionSettings() {
             }
         }
         _(), ImGui::DragFloat("Std Multiplier", &m_subdivCfg.stdMultiplier, 0.2f, 0, 10);
+        _(), ImGui::DragFloat("Risk Tolerance", &m_subdivCfg.riskTolerance, 0.2f, 0, 2.0);
         if (ImGui::Button("Clear CE Statistics")) {
             std::lock_guard lock_(m_mtx.field);
             m_field.ClearCEStatistics();
