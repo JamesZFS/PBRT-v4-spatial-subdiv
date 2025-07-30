8660f7471a717d0bfe87457f10aaf44da30eb60f
Fengshi Zheng <F.Zheng-1@tudelft.nl>
2025-07-31 01:53:26 +0200

    [multi-bin] implement simulation-based split criterion for multi-bin signatures

--- src/pbrt/guiding/guiding.cpp
+++ src/pbrt/guiding/guiding.cpp
@@ -726,7 +726,7 @@ std::unique_ptr<GuidedPathIntegrator> GuidedPathIntegrator::Create(
     else if (confidencetype == "risk") settings.treeconfidencetype = PGL_SPATIAL_CONFIDENCE_RISK;
     else if (confidencetype == "welch") settings.treeconfidencetype = PGL_SPATIAL_CONFIDENCE_TTEST;
     else if (confidencetype == "ttest_per_bin") settings.treeconfidencetype = PGL_SPATIAL_CONFIDENCE_TTEST_PER_BIN;
-    else if (confidencetype == "umvu") settings.treeconfidencetype = PGL_SPATIAL_CONFIDENCE_UMVU;
+    else if (confidencetype == "simulation") settings.treeconfidencetype = PGL_SPATIAL_CONFIDENCE_SIMULATION;
     else throw std::runtime_error("Unknown treeconfidencetype: " + confidencetype);
 
     auto defensivetype = parameters.GetOneString("treedefensivetype", "fixed");
