64bb734eb0d25a64b98f0f2c9c467b88c7d12650
Sebastian Herholz <sebastian.herholz@gmail.com>
2025-09-03 11:17:59 +0200

    Adapt on Ubuntu

--- setpath.sh
+++ setpath.sh
@@ -3,8 +3,8 @@
 PBRT_DIR=$( realpath $( dirname "${BASH_SOURCE[0]}" ))
 
 if [[ "$(uname)" == 'Darwin' ]]; then
-	export PATH="$PBRT_DIR/build:$PATH"
+	export PATH="$PBRT_DIR/cmake-build-release:$PATH"
 else
-	export LD_LIBRARY_PATH="$PBRT_DIR/build"
-	export PATH="$PBRT_DIR/build:$PATH"
+	export LD_LIBRARY_PATH="$PBRT_DIR/cmake-build-release"
+	export PATH="$PBRT_DIR/cmake-build-release:$PATH"
 fi
